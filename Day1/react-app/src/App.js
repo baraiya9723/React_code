e54ae2a7a3d075dd02aaafccd7a7b5fe94171679
baraiya9723 <vishalbaraiya583@gmail.com>
2024-12-22 12:57:38 +0530

    react and vite-project set up done

---
+++ Day1/react-app/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     Helllo react
+    </div>
+  );
+}
+
+export default App;
---
+++ Day1/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Hello vishal 
+        <button>submit</button>
+      </div>
+    </>
+  )
+}
+
+export default App
